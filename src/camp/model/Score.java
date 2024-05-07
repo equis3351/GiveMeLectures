55c33d33a4ba55f46e74973d7b782b010cf86cce
Woojin Kim <rladnwlsok@naver.com>
2024-05-07 11:38:19 +0900

    스코어 취소

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -2,12 +2,9 @@ package camp.model;
 
 public class Score {
     private String scoreId;
-    private int scoreNum;
-    private String scoreRank;
 
     public Score(String seq) {
         this.scoreId = seq;
-
     }
 
     // Getter
