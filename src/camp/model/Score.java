0b06b948bceee3145d893e4f619fe0656a0c5cd3
Woojin Kim <rladnwlsok@naver.com>
2024-05-07 11:34:58 +0900

    점수 내용 추가

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -2,14 +2,18 @@ package camp.model;
 
 public class Score {
     private String scoreId;
+    private int scoreNum;
+    private String scoreRank;
 
     public Score(String seq) {
         this.scoreId = seq;
+
     }
 
     // Getter
     public String getScoreId() {
         return scoreId;
     }
+
 
 }
